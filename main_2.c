52cbec842db4ef04e756e5c7c9dcb228cd65a16c
Omi Das Amlan <amland613@gmail.com>
2025-06-02 13:03:35 +0600

    Update main_2.c

--- main_2.c
+++ main_2.c
@@ -5,17 +5,13 @@ int main()
 {int  a;
 printf("Please input Age in number:");
 scanf("%d",&a);
-if(a<18){printf("you are a kid.\n");
+if(a<18){printf(" kid.\n");
 }
-else if(a>18){printf("you are a young.\n");
+else if(a>=18 && a<=30){printf("young.\n");
 }
-else if(a<30){printf("you are a young.\n");
+else if(a>=31 && a<=55){printf(" Adult.\n");
 }
-else if(a>31){printf("you are a Adult.\n");
+else{printf(" old.\n");
 }
-else if(a<55){printf("you are a Adult.\n");
-}
-else{printf("you are a old.\n");
-}
-    return 0;
+    return 0;
 }
